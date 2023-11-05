886aac00ed5c372b10f3e6735b72a415f3296ff3
Prashant3232 <131966917+Prashant3232@users.noreply.github.com>
2023-11-05 12:29:16 +0530

    Add files via upload

---
+++ src/Data.jsx
@@ -0,0 +1,146 @@
+const DATA = [
+    {
+        id : 0,
+        title : "Men Solid Round Neck Cotton Blend Yellow T-Shirt",
+        price : 750,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo1.png"
+    },
+    {
+        id : 1,
+        title : "Ezee Sleeves Men's Casual Lycra Pants",
+        price : 800,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo2.png"
+    },
+    {
+        id : 2,
+        title : "Mens California Republic Hooded Sweatshirt",
+        price : 950,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo3.png"
+    },
+    {
+        id : 3,
+        title : "Manfinity LEGND Men T-Shirt ",
+        price : 1050,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo4.png"
+    },
+    {
+        id : 4,
+        title : "Designer Women Men Clothing Fashion ",
+        price : 1150,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo5.png"
+    },
+    {
+        id : 5,
+        title : "Men's Denim Jacket",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo6.png"
+    },
+    {
+        id : 6,
+        title : "A-Shirt Tops Vest T-shirt Tank Top Workout",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo7.png"
+    },
+
+    {
+        id : 7,
+        title : "Embroidered Blue Indowestern",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo8.png"
+    },
+    {
+        id : 8,
+        title : "Indian Kurta Clothing Fashion Shirt Mens Short Kurta",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo9.png"
+    },
+    {
+        id : 9,
+        title : "Fancy Kids Wear",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo10.png"
+    },
+    {
+        id : 10,
+        title : "Fashion MAMA'S Boys Cloths ",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo11.png"
+    },
+    {
+        id : 11,
+        title : "Fashion Design Winter Men Jacket",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo12.png"
+    },
+    {
+        id : 12,
+        title : "Princess Dresses For Girls",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo13.png"
+    },
+    {
+        id : 13,
+        title : "A Look Back At Baby Cloths ",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo14.png"
+    },
+    {
+        id : 14,
+        title : "Baby Girl Cloths Toddler Girl..",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo15.png"
+    },
+    {
+        id : 15,
+        title : "Plain Pleated Pants Fancy Ladies Trousers",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo16.png"
+    },
+    {
+        id : 16,
+        title : "Fabric: Crepe, Sleeve(Suhani Fashion)",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo17.png"
+    },
+    {
+        id : 17,
+        title : "Beautiful Grey Tissue Silk Designer Party Wear Saree",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo18.png"
+    },
+    {
+        id : 18,
+        title : "Cute New Baby Girl T-Shirt & Pants ",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo19.png"
+    },
+    {
+        id : 19,
+        title : "Red Wedding Saree Designs",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/photo20.png"
+    }
+   
+]
+
+export default DATA;
